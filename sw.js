52e37b5a774b1ab44655595a5065e44da47785c9
Romulo Beninca <rbeninca@gmail.com>
2026-05-21 11:19:09 -0300

    teste pwa

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'tabuada-ninja-v1';
-const ASSETS = ['./', './index.html', './icon.svg', './manifest.json'];
+const ASSETS = ['./', './index.html', './icone.png', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
